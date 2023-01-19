c6a039a8b7f523fbc460b63f8b1f00d5e210bb4a
Wesley Breno <74777926+Wesley-Breno@users.noreply.github.com>
2023-01-19 13:13:02 -0300

    Delete PycharmProjects/EstudandoDjango directory

--- PycharmProjects/EstudandoDjango/accounts/form.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Contato
-
-
-class ContatoForm(ModelForm):
-    class Meta:
-        model = Contato
-        fields = ['nome', 'sobrenome', 'telefone', 'sobre', 'categoria', 'foto']
